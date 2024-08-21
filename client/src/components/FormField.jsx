a9af98f5ed42555ea22783179c1afeeec84e18f8
Ashok <ashokct.dev@gmail.com>
2024-08-21 17:39:03 +0530

    Implemented home and generate post page with basic logic and UI

---
+++ client/src/components/FormField.jsx
@@ -0,0 +1,35 @@
+import React from 'react'
+
+const FormField = ({ labelName, type, name, placeholder, value, handleChange, isSupriseMe, handleSupriseMe }) => {
+  return (
+    <>
+      <div className='flex items-center gap-2 mb-2'>
+        <label
+          htmlFor={name}
+          className='block-me font-medium text-gray-900'>
+          {labelName}
+        </label>
+        {isSupriseMe && (
+          <button
+            type='button'
+            onClick={handleSupriseMe}
+            className='font-semibold text-xs bg-[#ECECF1] py-1 px-2 rounded-[5px] text-black'>
+            Suprise me
+          </button>
+        )}
+      </div>
+      <input
+        type={type}
+        id={name}
+        name={name}
+        value={value}
+        onChange={handleChange}
+        required
+        placeholder={placeholder}
+        className='bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-[#6469ff] focus:border-[#6469ff] outline-none block w-full p-3'
+      />
+    </>
+  )
+}
+
+export default FormField
